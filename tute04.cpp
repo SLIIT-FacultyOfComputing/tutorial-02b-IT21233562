d35302b283d899e725ecdbf18471b5bf1aaa98b8
IT21233562 <99631957+IT21233562@users.noreply.github.com>
2022-02-14 05:53:57 +0000

    done

--- tute04.cpp
+++ tute04.cpp
@@ -13,17 +13,43 @@ Do not modify the main function.*/
 
 #include <iostream>
 
-long Factorial(int no);
-long nCr(int n, int r);
+int fact(int num)
+{
+    int k = 1, i;
 
-int main() {
+    if (num == 0)
+    {
+        return(k);
+    }
+    else
+    {
+        for (i = 1; i <= num; i++)
+    {
+            k = k * i;
+	}
+    }
+    return (k);
+}
+long nCr(int n, int r)
+{
+  int ncr_var;
+
+  ncr_var = fact(n) / (fact(r) * fact(n - r));
+
+  return ncr_var;
+}
+
+int main() 
+{
   int n, r;
-  std::cout << "Enter a value for n ";
+  std::cout << "Enter a value for n :";
   std::cin >> n;
-  std::cout << "Enter a value for r ";
+  std::cout << "Enter a value for r :";
   std::cin >> r;
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
+
   std::cout << std::endl;
+
   return 0;
 }
