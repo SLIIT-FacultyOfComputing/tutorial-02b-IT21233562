397746879ccee7486a0f41e05cf873e160d7e581
IT21233562 <99631957+IT21233562@users.noreply.github.com>
2022-02-14 05:25:25 +0000

    done

--- tute02.cpp
+++ tute02.cpp
@@ -16,22 +16,19 @@ Please Note that the input command in C++ is std::cin. This is a representation 
 
 int main()
 {
-  
-}
-/*#include <stdio.h>
-int main()
-{
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
+  double salary, netSalary;
+  int etype, otHrs, otRate;
+
+  std::cout <<"Enter Employee Type : ";
+  std::cin >> etype;
+  std::cout <<"Enter Salary : ";
+  std::cin >> salary;
+  std::cout <<"Enter otHours : ";
+  std::cin >> otHrs;
+
+  switch(etype)
+  {
+    case 1 :
           otRate = 1000;
           break;
       case 2 :
@@ -40,11 +37,10 @@ int main()
       default :
           otRate = 1700;
           break;
-   }
-
-
+  }
    netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
-  
+   std::cout <<"Net Salary is " << netSalary << std:: endl;
+
    return 0;
-}*/
+}
+
